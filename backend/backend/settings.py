6cce5efa30a233f6e06e33f3a57c95f00717a44d
Кирилл <kirill@DESKTOP-VLJ27MU>
2024-09-22 10:49:10 +0300

    Add tests to workflows.v2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['188.123.230.70', 'localhost']
+ALLOWED_HOSTS = ['158.160.29.154', 'taskivodolaski.servebeer.com', 'localhost']
 
 
 # Application definition
